4b91864526e39981bcf652d7ebe418a57db66935
Mike Rhodes <mike.rhodes@gmail.com>
2023-09-06 21:04:45 +0100

    feat: post-process with deno

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 use regex::Regex;
-use std::io::{self, BufRead, Write};
+use std::{
+    io::{self, BufRead, Write},
+    process::{Command, Stdio},
+};
 
 fn main() {
-    let r = process(&mut io::stdin().lock(), &mut io::stdout().lock());
-    if let Err(err) = r {
-        println!("Error: {}", err);
-        std::process::exit(1)
-    }
+    let mut deno = Command::new("deno")
+        .args(["fmt", "--ext", "md", "-"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .spawn()
+        .expect("Failed to start sed process");
+
+    // If the deno process fills its stdout buffer, it may end up
+    // waiting until the parent reads the stdout, and not be able to
+    // read stdin in the meantime, causing a deadlock.
+    // Writing from another thread ensures that stdout is being read
+    // at the same time, avoiding the problem.
+    // https://doc.rust-lang.org/std/process/index.html.
+    let mut stdin = deno.stdin.take().expect("Failed to open stdin");
+    std::thread::spawn(move || {
+        let r = process(&mut io::stdin().lock(), &mut stdin);
+        if let Err(err) = r {
+            println!("Error: {}", err);
+            std::process::exit(1)
+        }
+    });
+
+    deno.wait().expect("deno died");
 }
 
 fn process(src: &mut dyn BufRead, dst: &mut dyn Write) -> Result<(), io::Error> {
